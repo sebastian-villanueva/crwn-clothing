27c349a73726773fa168811a11129e39e2d1df1c
sebastian-villanueva <sebastian.villanueva.castrillon@gmail.com>
2019-08-30 11:46:50 -0500

    update menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './Pages/homepage/homepage.component'
+import './App.css';
+import HomePage from './Pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
