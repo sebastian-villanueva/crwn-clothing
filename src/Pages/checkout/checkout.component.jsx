ab4ea859607f193dcbfbc82314155016c735396d
sebastian-villanueva <sebastian.villanueva.castrillon@gmail.com>
2019-09-18 11:02:34 -0500

    updating stripe integration and updating our favicon and title

--- src/Pages/checkout/checkout.component.jsx
+++ src/Pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../Components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../Components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -35,6 +36,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/20 - cvv: 123
+        </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 )
 
